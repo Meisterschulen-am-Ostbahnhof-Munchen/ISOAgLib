8315aa2bdb87e102c2d5f053fd3f2059c949dae6
Martin Wodok <m.wodok@osb-ag.de>
2005-03-15 16:06:24 +0000

    Removed iGetyPos.sh file which was just a temporary shell script and does NOT belong to the distribution of IsoAgLib...

--- xgpl_src/iGetyPos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for InterfaceSouce in `find . \( -name "i*.h" -o -name "i*.hpp" \) -and -not -path "*/impl/*"` ; do
-	echo Datei $InterfaceSouce
-
-	perl -p -i -e 's/GetyPos_c/iGetyPos_c/g' $InterfaceSouce
-	perl -p -i -e 's/iiGetyPos_c/iGetyPos_c/g' $InterfaceSouce
-#	exit 0
-done
