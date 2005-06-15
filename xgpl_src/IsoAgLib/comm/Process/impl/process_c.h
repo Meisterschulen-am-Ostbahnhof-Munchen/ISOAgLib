ea643f526943e6cae2831282567d5c8dead3ec40
torborg <torborg@unknown>
2005-06-15 16:59:47 +0000

    Updates to get to work with A1 build

--- xgpl_src/IsoAgLib/comm/Process/impl/process_c.h
+++ xgpl_src/IsoAgLib/comm/Process/impl/process_c.h
@@ -101,7 +101,7 @@
 #include "../Remote/impl/procdataremotebase_c.h"
 #include "processpkg_c.h"
 
-#if defined(SYSTEM_PC) && !defined(SYSTEM_PC_VC)
+#if defined(SYSTEM_PC) && !defined(SYSTEM_PC_VC) && !defined(SYSTEM_A1)
   #include <ext/slist>
   namespace std { using __gnu_cxx::slist;};
 #else
