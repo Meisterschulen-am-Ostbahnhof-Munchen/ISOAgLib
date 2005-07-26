e19f4288dd709d8b06e6d55e4776a30631db2fdb
spangler <spangler@unknown>
2005-07-26 07:23:21 +0000

    compare also against FUNCTION when layzy evaluation is used and this value isn't 0xFF at one side

--- xgpl_src/IsoAgLib/comm/SystemMgmt/ISO11783/impl/isoname_c.cpp
+++ xgpl_src/IsoAgLib/comm/SystemMgmt/ISO11783/impl/isoname_c.cpp
@@ -369,15 +369,21 @@ int8_t ISOName_c::higherPriThanPar(const uint8_t* rpb_compare) const
 
   // if one of the both comparison parameters have 0xFF in the byte 0..5, then only compare
   // device class, -instance and industry group
-  static const uint8_t lazyEvaluationIndicator[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-  if ( ( memcmp( pb_data, lazyEvaluationIndicator, 6 ) == 0 ) || ( memcmp( rpb_compare, lazyEvaluationIndicator, 6 ) == 0 ) )
+  static const uint8_t lazyEvaluationIndicator[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+  if ( ( memcmp( pb_data, lazyEvaluationIndicator, 5 ) == 0 ) || ( memcmp( rpb_compare, lazyEvaluationIndicator, 5 ) == 0 ) )
   { // perform only lazy evaluation
-    // compare device class
-    if ( (pb_data[6] >> 1) > (rpb_compare[6] >> 1) ) return -1;
-    else if ( (pb_data[6] >> 1) < (rpb_compare[6] >> 1) ) return +1;
     // compare device class instance and industry group
     if ( (pb_data[7] & 0x7F) > (rpb_compare[7] & 0x7F) ) return -1;
     else if ( (pb_data[7] & 0x7F) < (rpb_compare[7] & 0x7F) ) return +1;
+    // compare device class
+    if ( (pb_data[6] >> 1) > (rpb_compare[6] >> 1) ) return -1;
+    else if ( (pb_data[6] >> 1) < (rpb_compare[6] >> 1) ) return +1;
+    // check if one part has a NOT spefified FUNCTION ( == 0xFF as default value ), as then
+    // both sides should be regarded as equal for lazy evaluation
+    if ( ( pb_data[5] == 0xFF ) || ( rpb_compare[5] == 0xFF ) ) return 0;
+    // compare Function as both have a non-default value
+    if ( (pb_data[5]) > (rpb_compare[5]) ) return -1;
+    else if ( (pb_data[5]) < (rpb_compare[5]) ) return +1;
     // if still here -> both should be regarded as equal for this compare level
     return 0;
   }
