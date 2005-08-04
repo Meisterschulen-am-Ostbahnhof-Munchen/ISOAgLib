1b5ce0a17356968c0003be1672c79d0c53c01830
schmidt <schmidt@unknown>
2005-08-04 19:58:36 +0000

    The C2C can NOT compile the uint64_t additions because it does NOT understand what a long long is. At least this is the case with Tasking version 7.5 r 6.
    So, I commented this out for the C2C.

--- xgpl_src/IsoAgLib/hal/c2c/typedef.h
+++ xgpl_src/IsoAgLib/hal/c2c/typedef.h
@@ -69,7 +69,9 @@ typedef int int16_t;
 typedef unsigned long uint32_t;
 typedef long int32_t;
 
-typedef unsigned long long int uint64_t;
-typedef long long int int64_t;
+// NOTE: The C2C can NOT compile this because it does NOT understand what a long long is.
+//       At least this was the case with Tasking version 7.5 r 6.
+//typedef unsigned long long int uint64_t;
+//typedef long long int int64_t;
 
 #endif
