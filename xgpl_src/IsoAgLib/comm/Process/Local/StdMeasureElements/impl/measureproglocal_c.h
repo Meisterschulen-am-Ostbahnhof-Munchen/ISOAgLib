af2f1dae339662c7e42e71bbac34a332a1283464
spangler <spangler@unknown>
2005-11-01 20:51:57 +0000

    fix include statement

--- xgpl_src/IsoAgLib/comm/Process/Local/StdMeasureElements/impl/measureproglocal_c.h
+++ xgpl_src/IsoAgLib/comm/Process/Local/StdMeasureElements/impl/measureproglocal_c.h
@@ -89,7 +89,7 @@
 /* *************************************** */
 /* ********** include headers ************ */
 /* *************************************** */
-#include "../../../StdMeasureElements/impl/measureprogbase_c.h"
+#include <IsoAgLib/comm/Process/StdMeasureElements/impl/measureprogbase_c.h>
 
 // Begin Namespace __IsoAgLib
 namespace __IsoAgLib {
