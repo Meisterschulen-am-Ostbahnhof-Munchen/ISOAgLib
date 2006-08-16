e0293253784aaf6fb6e46435dc1938ef34567c1c
Burkhard Fischer <b.fischer@osb-ag.de>
2006-08-16 13:31:41 +0000

    project specific return value for addDDI2ExistingProcData() (e.g. iProcDataRemote_c, iProcDataRemoteSimpleMeasure_c, ...)

--- xgpl_src/IsoAgLib/comm/Process/iprocess_c.h
+++ xgpl_src/IsoAgLib/comm/Process/iprocess_c.h
@@ -307,12 +307,29 @@ public:
     };
 
 #ifdef USE_ISO_11783
-#if 0
   // addDDI2ExistingProcData only possible for remote process data project (iProcDataRemote_c has to be defined)
   /** checks if a DDI can be added to a group and return ptr to proc data if successfully */
+#ifdef USE_PROC_REMOTE_STD
   iProcDataRemote_c* addDDI2ExistingProcData(uint16_t rui16_DDI, uint16_t rui_deviceElement, const iDevKey_c& rc_devKey, GeneralCommand_c::ValueGroup_t& ren_valueGroup, bool refb_isSetpoint)
   { return static_cast<iProcDataRemote_c*>(Process_c::addDDI2ExistingProcData(rui16_DDI, rui_deviceElement, rc_devKey, ren_valueGroup, refb_isSetpoint));};
 #endif
+#ifdef USE_PROC_REMOTE_SIMPLE_MEASURE
+  iProcDataRemoteSimpleMeasure_c* addDDI2ExistingProcData(uint16_t rui16_DDI, uint16_t rui_deviceElement, const iDevKey_c& rc_devKey, GeneralCommand_c::ValueGroup_t& ren_valueGroup, bool refb_isSetpoint)
+  { return static_cast<iProcDataRemoteSimpleMeasure_c*>(Process_c::addDDI2ExistingProcData(rui16_DDI, rui_deviceElement, rc_devKey, ren_valueGroup, refb_isSetpoint));};
+#endif
+#ifdef USE_PROC_REMOTE_SIMPLE_SETPOINT
+  iProcDataRemoteSimpleSetpoint_c* addDDI2ExistingProcData(uint16_t rui16_DDI, uint16_t rui_deviceElement, const iDevKey_c& rc_devKey, GeneralCommand_c::ValueGroup_t& ren_valueGroup, bool refb_isSetpoint)
+  { return static_cast<iProcDataRemoteSimpleSetpoint_c*>(Process_c::addDDI2ExistingProcData(rui16_DDI, rui_deviceElement, rc_devKey, ren_valueGroup, refb_isSetpoint));};
+#endif
+#ifdef USE_PROC_REMOTE_SIMPLE_MEASURE_SETPOINT
+  iProcDataRemoteSimpleMeasureSetpoint_c* addDDI2ExistingProcData(uint16_t rui16_DDI, uint16_t rui_deviceElement, const iDevKey_c& rc_devKey, GeneralCommand_c::ValueGroup_t& ren_valueGroup, bool refb_isSetpoint)
+  { return static_cast<iProcDataRemoteSimpleMeasureSetpoint_c*>(Process_c::addDDI2ExistingProcData(rui16_DDI, rui_deviceElement, rc_devKey, ren_valueGroup, refb_isSetpoint));};
+#endif
+#ifdef USE_PROC_REMOTE_SIMPLE_MEASURE_SETPOINT_COMBINED
+  iProcDataRemoteSimpleMeasureSetpointCombined_c* addDDI2ExistingProcData(uint16_t rui16_DDI, uint16_t rui_deviceElement, const iDevKey_c& rc_devKey, GeneralCommand_c::ValueGroup_t& ren_valueGroup, bool refb_isSetpoint)
+  { return static_cast<iProcDataRemoteSimpleMeasureSetpointCombined_c*>(Process_c::addDDI2ExistingProcData(rui16_DDI, rui_deviceElement, rc_devKey, ren_valueGroup, refb_isSetpoint));};
+#endif
+
   /** checks if a DDI can be added to a group and if yes then add it! */
   bool checkAndAddMatchingDDI2Group(uint16_t rui16_DDI, uint16_t rui_deviceElement, const iDevKey_c& rc_devKey)
   { return Process_c::checkAndAddMatchingDDI2Group(rui16_DDI, rui_deviceElement, rc_devKey);};
