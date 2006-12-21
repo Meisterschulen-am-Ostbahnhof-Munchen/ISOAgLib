6a07efad05f7b471a069f2d920bba3e7b03748b3
spangler <spangler@unknown>
2006-12-21 13:18:28 +0000

    Forward port of r2253 | spangler | 2006-12-14 15:18:43 +0100 (Do, 14 Dez 2006) | 1 line identify all TP,ETP idents also as high load

--- xgpl_src/Application_Config/isoaglib_config.h
+++ xgpl_src/Application_Config/isoaglib_config.h
@@ -171,9 +171,9 @@
 
 #ifndef CONFIG_CAN_HIGH_LOAD_IDENT_LIST
   /// define list of CAN filters where large buffers are needed due to high load
-  #define CONFIG_CAN_HIGH_LOAD_IDENT_LIST { 0xcb0000 }
+  #define CONFIG_CAN_HIGH_LOAD_IDENT_LIST { 0xc70000, 0xc80000, 0xcb0000, 0xeb0000, 0xec0000  }
   /// define amount of list entries in CONFIG_CAN_HIGH_LOAD_IDENT_LIST
-  #define CONFIG_CAN_HIGH_LOAD_IDENT_CNT 1
+  #define CONFIG_CAN_HIGH_LOAD_IDENT_CNT 5
   /// define mask for compare with ident
   #define CONFIG_CAN_HIGH_LOAD_IDENT_MASK 0xff0000
 #endif
