e5bf0e4e87850229d7e0c27eb1d221eb8825c746
jaekel <jaekel@unknown>
2007-08-06 12:55:08 +0000

    Change of documentation

--- IsoAgLib_Examples/tutorial/2_ProcessData/2_9_RemoteWriteSetpointStdIso/2_9_RemoteWriteSetpointStdIso.cpp
+++ IsoAgLib_Examples/tutorial/2_ProcessData/2_9_RemoteWriteSetpointStdIso/2_9_RemoteWriteSetpointStdIso.cpp
@@ -51,14 +51,23 @@
 
 /* *********************************************************************** */
 /** \example 2_9_RemoteWriteSetpointStdIso.cpp
- * This tutorial shall provide a simple base program, which creates some
- * remote process data, which are used to control the remote device.
- * Demonstrate optional usage of handler class with a method which is automatically
- * called on each setpoint response receive.
+ * In section 2 tutorial examples the provision and distribution of process 
+ * values is demonstrated. This communication is done over CAN-BUS. An 
+ * example consists of a pair of two applications. One 
+ * application is ment as local process (*_Local*), another is ment as remote 
+ * process (*_Remote*). If an example provides sole measurment values it is 
+ * grouped in a read example (*Read*). If the remote application sets values
+ * in a local application, it is gouped in a write example (*Write*).
+ *
+ * 
  *
  * <H1>What is demonstrated</H1>
  * <ol>
- * <li>Create some remote standard process data and request measurement data from remote device
+ * <li> This example ("2_9_RemoteWriteSetpointStdIso") shows how to send 
+ * setpoints to distant processes and demonstrates the optional usage 
+ * of a handler class with a method which is automatically called on each 
+ * setpoint response receive. An example for data provisioning can be
+ * fount in 2_4_LocalWriteSetpointStdIso. Important used concepts are:
  * <ul>
  *  <li>Standard remote process data class IsoAgLib::iProcDataRemote_c
  *  <li>Use constructor IsoAgLib::iProcDataRemote_c::iProcDataRemote_c to create variable with defined property
