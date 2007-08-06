5b7f7bfcf781cb9882336b65c3a0cbd30d4db15f
jaekel <jaekel@unknown>
2007-08-06 12:54:32 +0000

    Change of documentation

--- IsoAgLib_Examples/tutorial/2_ProcessData/2_5_LocalWriteSetpointSimpleMeasureIso/2_5_LocalWriteSetpointSimpleMeasureIso.cpp
+++ IsoAgLib_Examples/tutorial/2_ProcessData/2_5_LocalWriteSetpointSimpleMeasureIso/2_5_LocalWriteSetpointSimpleMeasureIso.cpp
@@ -51,21 +51,28 @@
 
 /* *********************************************************************** */
 /** \example 2_5_LocalWriteSetpointSimpleMeasureIso.cpp
- * This tutorial shall provide the simples base program, which creates some
- * local process data, which value is regularly updated.
- * Remote ECUs can only request single values, as this variant uses the
- * simple process data type iProcDataLocalSimpleMeasure_c.
- * This variant is enough if other remote ECUs don't start measurement programs
- * and reduction of memory need is important.
+ * In section 2 tutorial examples the provision and distribution of process 
+ * values is demonstrated. This communication is done over CAN-BUS. An 
+ * example consists of a pair of two applications. One 
+ * application is ment as local process (*_Local*), another is ment as remote 
+ * process (*_Remote*). If an example provides sole measurment values it is 
+ * grouped in a read example (*Read*). If the remote application sets values
+ * in a local application, it is gouped in a write example (*Write*).
+ *
  *
  * <H1>What is demonstrated</H1>
  * <ol>
- * <li>Create some local process data which don't support measurement programs
- *    and update their data so that remote device can only perform one shot
- *    data requests ( start of a periodical send of data isn't supported -
- *    look at IsoAgLib::iProcDataLocalSimpleMeasure_c for this )
- * <li>Use the posibility to check for device type of setpoints sender to decide on
- *     accept or deny of setpoint value
+ * <li>This example ("2_5_LocalWriteSetpointSimpleMeasureIso") creates some examplary 
+ * local process data. This process data is regualarly updated. The values are 
+ * stored in an IsoAgLib::iProcDataLocalSimpleMeasure_c object. These values 
+ * can be requested by remote processes over CAN-BUS communication. This stored data
+ * does not support measurement programs. Remote devices can only perform one 
+ * shot data requests ( start of a periodical send of data isn't supported -
+ *    look at IsoAgLib::iProcDataLocal_c for this ). Furthermore this application 
+ * allowes remote processes to define setpoints. An example for this request 
+ * can be fount in 2_9_RemoteWriteSetpointStdIso. The following concepts are used 
+ * to check for the device type of a setpoint's sender in order to
+ * decide whether to accept or deny a setpoint value:
  * <ul>
  *  <li>Standard local process data class IsoAgLib::iProcDataLocalSimpleMeasure_c
  *  <li>Use constructor IsoAgLib::iProcDataLocalSimpleMeasure_c::iProcDataLocalSimpleMeasure_c to create variable with defined property
