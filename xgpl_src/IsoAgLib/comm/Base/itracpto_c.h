ab965404a41d1917711344b7ddcf60682467a62b
souissi <souissi@unknown>
2007-09-18 15:34:06 +0000

    - Adapted the tracpto_c class to the changes already applied to the BaseCommon_c class in the former commit

--- xgpl_src/IsoAgLib/comm/Base/itracpto_c.h
+++ xgpl_src/IsoAgLib/comm/Base/itracpto_c.h
@@ -84,8 +84,8 @@ namespace IsoAgLib {
       @param rt_identMode set mode to either IsoAgLib::IdentModeImplement or IsoAgLib::IdentModeTractor
       @return true -> configuration was successfull
     */
-  bool config (const iISOName_c* rpc_isoName, IsoAgLib::IdentMode_t rt_identMode = IsoAgLib::IdentModeImplement)
-  { return TracPTO_c::config_base (rpc_isoName, rt_identMode ); }
+  bool config (const iISOName_c* rpc_isoName,uint16_t rui16_suppressMask = 0, IsoAgLib::IdentMode_t rt_identMode = IsoAgLib::IdentModeImplement)
+  { return TracPTO_c::config_base (rpc_isoName, rui16_suppressMask, rt_identMode ); }
 
 /* ******************************************* */
     /** \name Set Values for periodic send on BUS  */
