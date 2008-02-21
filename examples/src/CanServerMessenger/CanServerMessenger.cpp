f282b574b3eb2a0895e95e34203fdfef26574d74
Martin Wodok <m.wodok@osb-ag.de>
2008-02-21 14:21:36 +0000

    CanServerMessenger: Added usage-output and parameter '-s' can now also be given using '-d' as equivalent.

--- examples/src/CanServerMessenger/CanServerMessenger.cpp
+++ examples/src/CanServerMessenger/CanServerMessenger.cpp
@@ -1,6 +1,6 @@
 /**
  * \example CanServerMessenger.cpp
- * CanServerMessenger - based on IsoAgLib and "rte_can" (c) Manfred Guenzl
+ * CanServerMessenger - based on IsoAgLib and "rte_can"
  * <H1>Howto compile this example</H1>
  * <ol>
  * <li>Call the script update_makefile.sh with the spec file
@@ -119,11 +119,14 @@ public:
 
 private:
 
-  void usage() const;
+  int ahextoi (char* str);
+  int databytes (char* src, uint8_t* dst);
+
+  void usage_and_exit(int ai_errorCode) const;
 };
 
 
-int ahextoi (char* str)
+int cmdline_c::ahextoi (char* str)
 {
   int value=0;
   char x;
@@ -133,13 +136,13 @@ int ahextoi (char* str)
     else if ((x >= '0') && (x <= '9')) value = (value << 4) | (x - '0');
     else if ((x >= 'A') && (x <= 'F')) value = (value << 4) | (x - 'A' + 10);
     else if ((x >= 'a') && (x <= 'f')) value = (value << 4) | (x - 'a' + 10);
-    else { printf ("Hex number error!\n"); exit(1); }
+    else { printf ("Hex number error!\n"); usage_and_exit(1); }
   }
   return value;
 }
 
 
-int databytes (char* src, uint8_t* dst)
+int cmdline_c::databytes (char* src, uint8_t* dst)
 {
   int bytes=0;
   bool first=true;
@@ -150,7 +153,7 @@ int databytes (char* src, uint8_t* dst)
     /**/ if ((x >= '0') && (x <= '9')) value = (x - '0');
     else if ((x >= 'A') && (x <= 'F')) value = (x - 'A' + 10);
     else if ((x >= 'a') && (x <= 'f')) value = (x - 'a' + 10);
-    else { printf ("Hex number error!\n"); exit(1); }
+    else { value=0; printf ("Hex number error!\n"); usage_and_exit(1); }
     if (first)
     {
       *dst = value << 4;
@@ -167,7 +170,7 @@ int databytes (char* src, uint8_t* dst)
   if (!first)
   {
     printf ("last data byte incomplete!!\n");
-    exit(1);
+    usage_and_exit(1);
   }
   return bytes;
 }
@@ -176,14 +179,15 @@ int databytes (char* src, uint8_t* dst)
 void cmdline_c::parse (int argc, char *argv[])
 {
   char nextup [] = "  ";
+  if (argc < 2) usage_and_exit(1);
   for (int i=1; i<argc; i++)
   {
     if (nextup[2-1] == ' ')
     {
       if (argv[i][1-1] != '-')
       { // error
-        printf ("Specify parameter type please!");
-        exit(1);
+        printf ("Specify parameter type please!\n");
+        usage_and_exit(1);
       }
       nextup[2-1] = argv[i][2-1];
       continue;
@@ -195,10 +199,11 @@ void cmdline_c::parse (int argc, char *argv[])
         case 'i': i_id = ahextoi(argv[i]); break;
         case 'n': i_repeat = atoi(argv[i]); break;
         case 'c': i_channel = atoi (argv[i]); break;
-        case 's': i_databytes = databytes(argv[i], pui8_databytes); break;
+        case 's':
+        case 'd': i_databytes = databytes(argv[i], pui8_databytes); break;
         case 'p': i_period = atoi (argv[i]); break;
-				case 'x': b_ext = true; i--; break;
-        default: printf ("Unsupported parameter!\n"); exit(1); break;
+        case 'x': b_ext = true; i--; break;
+        default: printf ("Unsupported parameter!\n"); usage_and_exit(1); break;
       }
       nextup[2-1] = ' '; // free to await next parameter-pair
       continue;
@@ -207,10 +212,23 @@ void cmdline_c::parse (int argc, char *argv[])
   if (nextup[2-1] != ' ')
   { // didn't finish parameter-pair ==> error
     printf ("Incomplete parameter-type\n");
-    exit (1);
+    usage_and_exit (1);
   }
 }
 
+
+void cmdline_c::usage_and_exit (int ai_errorCode) const
+{
+  printf ("\nCommandline-parameters are:\n   -i <CAN-Identifier as HEX>\n");
+  printf ("   -n <Repeat-Count>\n");
+  printf ("   -c <CAN-Channel>\n");
+  printf ("   -d <Databytes (up to 8) as HEX> (for compatibility also -s can be used)\n");
+  printf ("   -p <Period in ms>\n");
+  printf ("   -x     (use eXtended Identifier)\n");
+  printf ("\n Example: CanServerMessenger -x -n 1 -c 0 -i 1ceafffe -d a1b2c3d4e5f6affe\n\n");
+
+  exit (ai_errorCode);
+}
 
 
 using namespace IsoAgLib;
@@ -221,10 +239,11 @@ int main( int argc, char *argv[] )
 
   params.parse (argc, argv);
 
-  printf ("Sending on CAN-Bus %d %d times with a period of %d: ID=%x (%dx)databytes %x %x %x %x %x %x %x %x\n",
-          params.i_channel,         params.i_repeat,          params.i_period,          params.i_id, params.i_databytes,
-          params.pui8_databytes[0], params.pui8_databytes[1], params.pui8_databytes[2], params.pui8_databytes[3],
-          params.pui8_databytes[4], params.pui8_databytes[5], params.pui8_databytes[6], params.pui8_databytes[7]);
+  printf ("Sending on CAN-Bus %d %d times with a period of %d: ID=%x [%d databytes:] ", params.i_channel, params.i_repeat, 
+                                                                                        params.i_period,  params.i_id,
+                                                                                        params.i_databytes);
+  for (int i=0; i<params.i_databytes; i++) printf (" %02x", params.pui8_databytes[i]);
+  printf ("\n");
 
   // Initialize CAN-Bus
   getIcanInstance().init (params.i_channel); // CAN-Bus (with defaulting 250 kbit)
