e1104b08fac38e3bfbd338caf14272deec12fef8
Martin Wodok <m.wodok@osb-ag.de>
2008-10-07 08:22:08 +0000

    VT-Client: Feature extension: Added Revision 4 feature to set focus to Button-objects. !!! -> Use with care and ONLY ON REVISION4-capable VTs. Reviewed by Antoine Kandera

--- library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/ivtobjectbutton_c.h
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/ivtobjectbutton_c.h
@@ -79,7 +79,14 @@ public:
   //  Operation: get_vtObjectButton_a
   const iVtObjectButton_s& get_vtObjectButton_a();
 
-
+  //  Operation: select
+  //  ATTENTION: THIS IS ONLY SUPPORTED BY REVISION 4 VTs
+  //  WHICH ARE CURRENTLY NOT YET ON THE MARKET.
+  //  SO USE THIS COMMAND ONYL FOR SPECIAL PURPOSES AND IF YOU KNOW YOUR VT-SERVER
+  //  SUPPORTS THIS COMMAND - OTHER THAN THAT YOU WILL GET 4,5 SECONDS OF DELAY IN YOUR COMMUNICATION!
+  //  @todo Put this command into some #ifdef REVISION3_CLIENT.
+  //  @todo Change #ifdef REVISION3_CLIENT to #ifdef REVISION4_CLIENT
+  bool select(uint8_t selectOrActivate) { return vtObject_c::select(selectOrActivate); }
 
 
   /// Operation: setSize
