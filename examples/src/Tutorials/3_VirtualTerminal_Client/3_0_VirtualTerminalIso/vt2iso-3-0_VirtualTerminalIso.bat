b54b20f40d81b62960a873abd8980b32e378bdbe
hafner <hafner@unknown>
2009-05-12 12:43:57 +0000

    Updated Windows canserver binaries, vt2iso wrapper scripts, VCPROJ files.
    Reviewed by Martin Wodok.

--- examples/src/Tutorials/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/vt2iso-3-0_VirtualTerminalIso.bat
+++ examples/src/Tutorials/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/vt2iso-3-0_VirtualTerminalIso.bat
@@ -1,1 +1,1 @@
-vt2iso\Vt2Iso.exe  -bitmapmemory=HUGE_MEM ..\..\..\examples\src\Tutorials\3_VirtualTerminal_Client\3_0_VirtualTerminalIso\MaskDefinition\simpleVTIsoPool"
+..\..\..\..\..\bin\win32\vt2iso.exe "..\..\..\..\..\examples\src\Tutorials\3_VirtualTerminal_Client\3_0_VirtualTerminalIso\MaskDefinition\simpleVTIsoPool"
