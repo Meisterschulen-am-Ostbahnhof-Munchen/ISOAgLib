0084c0c89eca5950182bd378bc25f8d9db6a95e6
hafner <hafner@unknown>
2009-05-12 11:56:00 +0000

    Fixed relative path to vt2iso.

--- examples/src/Tutorials/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/vt2iso-3-0_VirtualTerminalIso.sh
+++ examples/src/Tutorials/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/vt2iso-3-0_VirtualTerminalIso.sh
@@ -9,8 +9,8 @@
 ### if your xerces-c is NOT located in /usr/local/lib and can't be found by vt2iso, change the path in (3) also.
 
 ### 1. Save path to where this script lies.
-CURDIR=`pwd`
-DIR=`dirname $0`
+CURDIR="$PWD"
+DIR="$(dirname "$0")"
 
 ### 2. Change to where to XML definitions are...
 # cd $CURDIR/$DIR/../examples/tutorial/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/MaskDefinition
@@ -19,8 +19,9 @@ DIR=`dirname $0`
 # export LD_LIBRARY_PATH=/usr/local/lib
 
 ### 4. Now call vt2iso
-# PAR="$CURDIR/$DIR/vt2iso/bin/vt2iso simpleVTIsoPool.xml"
+# PAR=("$CURDIR/$DIR/vt2iso/bin/vt2iso" "simpleVTIsoPool.xml")
 
-PAR="$CURDIR/$DIR/../../..//bin/vt2iso $CURDIR/$DIR/../../../examples/src/Tutorials/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/MaskDefinition/simpleVTIsoPool"
+PAR=("$DIR/../../../../../bin/x86linux/vt2iso"
+    "$DIR/../../../../../examples/src/Tutorials/3_VirtualTerminal_Client/3_0_VirtualTerminalIso/MaskDefinition/simpleVTIsoPool")
 
-exec $PAR
+exec "${PAR[@]}"
