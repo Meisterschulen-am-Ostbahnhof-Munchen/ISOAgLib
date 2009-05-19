8e6655785b6f565689796adf4a301814bd7414d4
Martin Wodok <m.wodok@osb-ag.de>
2009-05-19 12:46:24 +0000

    [Cleanup] [HAL-PC-Errorcodes] [Only improved readability on the error-code-DEFINEs]
    Only improved readability on the error-code-DEFINEs
    No functional change.
    
    Post-review by Thomas Hafner

--- library/xgpl_src/IsoAgLib/hal/pc/errcodes.h
+++ library/xgpl_src/IsoAgLib/hal/pc/errcodes.h
@@ -55,31 +55,31 @@
 #include "typedef.h"
 
 namespace HAL {
-   /* --- GLOBAL DEFINES CONSTANTS --- */
-   /* Cause */
-   #define HAL_NO_ERR         (int16_t)( (0 * 0x0001))
-   #define HAL_UNKNOWN_ERR    (int16_t)(-(1 * 0x0001))
-   #define HAL_WARN_ERR       (int16_t)(-(2 * 0x0001))
-   #define HAL_DEFAULT        (int16_t)(-(3 * 0x0001))
-   #define HAL_BUSY_ERR       (int16_t)(-(4 * 0x0001))
-   #define HAL_RANGE_ERR      (int16_t)(-(5 * 0x0001))
-   #define HAL_OVERFLOW_ERR   (int16_t)(-(6 * 0x0001))
-   #define HAL_RD_WR          (int16_t)(-(7 * 0x0001))
-   #define HAL_NOACT_ERR      (int16_t)(-(8 * 0x0001))
-   #define HAL_COM            (int16_t)(-(9 * 0x0001))
-   #define HAL_CONFIG_ERR     (int16_t)(-(10* 0x0001))
-   #define HAL_CHECKSUM       (int16_t)(-(11* 0x0001))
-  
-  #define HAL_WD_DATA_CHANGED   1
-  
-  #define HAL_DIGOUT_OPEN        (int16_t)(-(20 * 0x0001))
-  #define HAL_DIGOUT_SHORTCUT    (int16_t)(-(21 * 0x0001))
-  #define HAL_DIGOUT_OVERTEMP    (int16_t)(-(22 * 0x0001))
-  #define HAL_DIGOUT_UNDERVOLT   (int16_t)(-(23 * 0x0001))
-  #define HAL_DIGOUT_OVERVOLT    (int16_t)(-(24 * 0x0001))
 
-  #define HAL_NEW_SEND_DELAY     (int16_t)(-(25 * 0x0001))
+/* --- GLOBAL DEFINES CONSTANTS --- */
+/* Cause */
+#define HAL_NO_ERR         ((int16_t) 0 )
+#define HAL_UNKNOWN_ERR    ((int16_t)-1 )
+#define HAL_WARN_ERR       ((int16_t)-2 )
+#define HAL_DEFAULT        ((int16_t)-3 )
+#define HAL_BUSY_ERR       ((int16_t)-4 )
+#define HAL_RANGE_ERR      ((int16_t)-5 )
+#define HAL_OVERFLOW_ERR   ((int16_t)-6 )
+#define HAL_RD_WR          ((int16_t)-7 )
+#define HAL_NOACT_ERR      ((int16_t)-8 )
+#define HAL_COM            ((int16_t)-9 )
+#define HAL_CONFIG_ERR     ((int16_t)-10)
+#define HAL_CHECKSUM       ((int16_t)-11)
+
+#define HAL_WD_DATA_CHANGED   1
+
+#define HAL_DIGOUT_OPEN        ((int16_t)-20 )
+#define HAL_DIGOUT_SHORTCUT    ((int16_t)-21 )
+#define HAL_DIGOUT_OVERTEMP    ((int16_t)-22 )
+#define HAL_DIGOUT_UNDERVOLT   ((int16_t)-23 )
+#define HAL_DIGOUT_OVERVOLT    ((int16_t)-24 )
+
+#define HAL_NEW_SEND_DELAY     ((int16_t)-25 )
+
 } // end namespace __HAL
 #endif
-
-
