be54bfd5cd1b9f44aeec9bcb08cbe8a62f457814
Martin Wodok <m.wodok@osb-ag.de>
2009-09-16 11:47:31 +0000

    [Fix] [SocketCAN] [Cleanup on return statement that was missing in release version]
    - And some other cleanup to get it compiled properly, including some perror(..) calls.
    - Baudrate will only be set at Kernel prior to 2.6.
    - closeBusOnCard implemented.
    - setting defaults to ensure they are porperly set up in case this default changes.
    
    Done by F. Obenhuber
    Reviewed by B. Fischer & M. Wodok

--- tools/can_server/src/devices/can_device_socketcan.cpp
+++ tools/can_server/src/devices/can_device_socketcan.cpp
@@ -47,20 +47,18 @@
  * the main author Achim Spangler by a.spangler@osb-ag:de                  *
  ***************************************************************************/
 
+#include <errno.h>
+#include <string.h>
 #include <sys/time.h>
 #include <time.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
 #include <net/if.h>
-
-#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #include <linux/can.h>
 #include <linux/can/raw.h>
-
-#include <string.h>
 
 #include "can_server.h"
 #include "can_server_common.h"
@@ -76,20 +74,19 @@ using namespace __HAL;
   #define AF_CAN PF_CAN
 #endif
 
-/** 
- * Baudrate definition in bits per second 
- */
+extern int errno;
+
+/* prior to 2.5.x versions of socket theres a specific ioctrl for 
+ * setting the baudrate abailable. */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
 typedef uint32_t can_baudrate_t;
-
-// @todo SOCKET CAN: this define is /usr/include/linux/can/ioctl.h
 #define SIOCSCANBAUDRATE	(SIOCDEVPRIVATE+0)
-
-
-#undef DEBUG_CAN
+#endif
 
 #ifdef DEBUG_CAN
 std::ofstream m_clog;
 #endif
+
 
 static bool canBusIsOpen[cui32_maxCanBusCnt];
 
@@ -120,6 +117,8 @@ bool resetCard(void)
 // PURPOSE: To initialize the specified CAN BUS to begin sending/receiving msgs
 bool openBusOnCard(uint8_t ui8_bus, uint32_t wBitrate, server_c* pc_serverData)
 {
+    (void)wBitrate; // not used for Kernel >= 2.6.
+
     DEBUG_PRINT1("init can bus %d\n", ui8_bus);
 
     if( !canBusIsOpen[ui8_bus] ) 
@@ -132,7 +131,7 @@ bool openBusOnCard(uint8_t ui8_bus, uint32_t wBitrate, server_c* pc_serverData)
         // Create a socket for talking to the CAN Bus
         int m_handle = socket(PF_CAN, SOCK_RAW, CAN_RAW);
 #ifdef DEBUG_CAN
-    m_clog << "Init(): bus: " << (int)ui8_bus << "fname: " << fname << "m_handle: " << m_handle << std::endl;
+        m_clog << "Init(): bus: " << (int)ui8_bus << "fname: " << fname << "m_handle: " << m_handle << std::endl;
 #endif
         if (m_handle < 0)
         {
@@ -144,14 +143,17 @@ bool openBusOnCard(uint8_t ui8_bus, uint32_t wBitrate, server_c* pc_serverData)
         strncpy(ifr.ifr_name, fname, IFNAMSIZ);
         int ret = ioctl(m_handle, SIOCGIFINDEX, &ifr);
 #ifdef DEBUG_CAN
-    m_clog << "Init(): ret: " << ret << std::endl;
+        m_clog << "Init(): ret: " << ret << std::endl;
 #endif
         if (ret < 0)
         {
+            perror( "SIOCGIFINDEX" );
             pc_serverData->marri32_can_device[ui8_bus] = NULL;
             return false;
         }
 
+// Setting of Bitrate not possible in >= 2.6. Kernel version
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,0)
         // Set the baudrate.  Must be set before first use
         struct ifreq ifr_baudrate;   // Use different structure for setting the baud rate as the ioctls
                                     // overwrite the structure
@@ -160,25 +162,41 @@ bool openBusOnCard(uint8_t ui8_bus, uint32_t wBitrate, server_c* pc_serverData)
         *baudrate = wBitrate * 1000; 
         int baud_ret = ioctl(m_handle, SIOCSCANBAUDRATE, &ifr_baudrate);
 #ifdef DEBUG_CAN
-    m_clog << "Init(): baud_ret: " << baud_ret << std::endl;
+        m_clog << "Init(): baud_ret: " << baud_ret << std::endl;
 #endif
         if (baud_ret < 0)
         {
+            perror( "SIOCSCANBAUDRATE" );
             pc_serverData->marri32_can_device[ui8_bus] = NULL;
             return false;
         }
-
+#endif
         // Bind to the socket
         struct sockaddr_can addr;
         addr.can_family = AF_CAN;
         addr.can_ifindex = ifr.ifr_ifindex;
         int result = bind( m_handle, (struct sockaddr*)&addr, sizeof(addr) );
+
 #ifdef DEBUG_CAN
-    m_clog << "Init(): result: " << result << std::endl;
+        m_clog << "Init(): result: " << result << std::endl;
 #endif
         if (result < 0)
         {
+            perror( "bind" );
             pc_serverData->marri32_can_device[ui8_bus] = NULL;
+            return false;
+        }
+
+        // Enable loopback (ensure, because it may not be default in the future)
+        int loopback = 1; /* 0 = disabled, 1 = enabled (default) */
+        if( -1 == setsockopt( m_handle, SOL_CAN_RAW, CAN_RAW_LOOPBACK, &loopback, sizeof(loopback)) ) {
+            perror( "enabling loopback" );
+            return false;
+        }
+
+        int recv_own_msgs = 0; /* 0 = disabled (default), 1 = enabled */
+        if( -1 == setsockopt( m_handle, SOL_CAN_RAW, CAN_RAW_RECV_OWN_MSGS, &recv_own_msgs, sizeof(recv_own_msgs)) ) {
+            perror( "setting local echo option" );
             return false;
         }
 
@@ -191,11 +209,13 @@ bool openBusOnCard(uint8_t ui8_bus, uint32_t wBitrate, server_c* pc_serverData)
         return true; // already initialized and files are already open
 }
 
-void closeBusOnCard(uint8_t ui8_bus, server_c* /*pc_serverData*/)
+
+void closeBusOnCard(uint8_t ui8_bus, server_c* pc_serverData )
 {
-  DEBUG_PRINT1("close can bus %d\n", ui8_bus);
-  //canBusIsOpen[ui8_bus] = false;
-  // do not call close or CAN_CLOSE because COMMAND_CLOSE is received during initialization!
+  canBusIsOpen[ui8_bus] = false;
+  if( 0 > close( pc_serverData->marri32_can_device[ui8_bus] ) ) {
+      perror( "close" );
+  }
 }
 
 
@@ -209,7 +229,7 @@ void __HAL::updatePendingMsgs(server_c* /*pc_serverData*/, int8_t /*i8_bus*/)
 int16_t sendToBus(uint8_t ui8_bus, canMsg_s* ps_canMsg, server_c* pc_serverData)
 {
 #ifdef DEBUG_CAN
-        m_clog << "sendToBus(): bus: " << (int)ui8_bus << " isOpen: " << canBusIsOpen[ui8_bus] << std::endl;
+    m_clog << "sendToBus(): bus: " << (int)ui8_bus << " isOpen: " << canBusIsOpen[ui8_bus] << std::endl;
 #endif
     if ((ui8_bus < HAL_CAN_MAX_BUS_NR) && canBusIsOpen[ui8_bus])
     {
@@ -228,8 +248,9 @@ int16_t sendToBus(uint8_t ui8_bus, canMsg_s* ps_canMsg, server_c* pc_serverData)
         int bytes_sent = send(pc_serverData->marri32_can_device[ui8_bus], &frame, sizeof(frame), 0 );
         if (bytes_sent < 0)
         {
+            perror( "send" );
 #ifdef DEBUG_CAN
-        m_clog << "sendToBus(): error, bytes_sent: " << bytes_sent << std::endl;
+            m_clog << "sendToBus(): error, bytes_sent: " << bytes_sent << std::endl;
 #endif
             return 0;
         }
@@ -240,16 +261,18 @@ int16_t sendToBus(uint8_t ui8_bus, canMsg_s* ps_canMsg, server_c* pc_serverData)
         sprintf(buf,"sendToBus(): %X,%X,%d,%02X,%02X,%02X,%02X,%02X,%02X,%02X,%02X",
                 frame.can_id, ps_canMsg->i32_msgType, frame.can_dlc, frame.data[0], frame.data[1], frame.data[2], frame.data[3], frame.data[4], frame.data[5], frame.data[6], frame.data[7]);
         m_clog << buf << std::endl;
-
-        return bytes_sent;
 #endif
+        return bytes_sent;
     }
     else
     {
+        perror( "invalid call" );
         return 0;
     }
-    return 0;
+
+    return 0; // shouldn't reach here actually. only for compiler.
 }
+
 
 bool readFromBus(uint8_t ui8_bus, canMsg_s* ps_canMsg, server_c* pc_serverData)
 {
@@ -273,15 +296,24 @@ bool readFromBus(uint8_t ui8_bus, canMsg_s* ps_canMsg, server_c* pc_serverData)
 #ifdef DEBUG_CAN
         m_clog << "readFromBus(timeout): retval: " << retval << std::endl;
 #endif
-        if (retval <= 0)    // -1 on error, 0 on no data within timeout
+        if (retval <= 0)     // -1 on error, 0 on no data within timeout
+        {
+            if ( -1 == retval ) {
+                perror( "select" );
+            }
             return false;
+        }
 
         int status = recv(pc_serverData->marri32_can_device[ui8_bus], &frame, sizeof(frame), 0);
 #ifdef DEBUG_CAN
         m_clog << "readFromBus(): status: " << status << std::endl;
 #endif
-        if (status <= 0)
+        if (status <= 0) {
+            if ( -1 == status ) {
+                perror( "read" );
+            }
             return false;
+        }
 
         ps_canMsg->ui32_id = frame.can_id & CAN_ERR_MASK;
         for(int i = 0; i < 8; ++i)
