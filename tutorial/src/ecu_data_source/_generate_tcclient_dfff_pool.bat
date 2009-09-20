76ab00c20ad1ba1a12e9a8f37e76aca860f6bbfd
Martin Wodok <m.wodok@osb-ag.de>
2009-09-20 21:28:28 +0000

    [Enhancement][Tutorial ECU DataSource DFFF] [Now supporting Default Data Logging]
    Post-Review before 2.2 release.

---
+++ tutorial/src/ecu_data_source/_generate_tcclient_dfff_pool.bat
@@ -0,0 +1,4 @@
+cd component_tcclient_dfff_pool
+..\..\..\..\bin\win32\proc2iso.cmd tutorialDataSourceDFFF
+cd ..
+pause
--- tutorial/src/ecu_data_source/component_tcclient_dfff.h
+++ tutorial/src/ecu_data_source/component_tcclient_dfff.h
@@ -4,6 +4,9 @@
 // IsoAgLib
 #include <IsoAgLib/typedef.h>
 
+// IsoAgLib (DFFF-specific)
+#include <IsoAgLib/comm/Part7_ProcessData/processdatachangehandler_c.h>
+
 
 namespace IsoAgLibTutorialDataSource {
 
@@ -15,7 +18,8 @@ namespace IsoAgLibTutorialDataSource {
       virtual void handleNewSensorBData( int32_t ai32_newData ) = 0;
   };
 
-  class TutorialDataSource_c : public iDataSource_c {
+  // DFFF-specific: also derive from ProcessDataChangeHandler_c to receive DFFF-requests
+  class TutorialDataSource_c : public iDataSource_c, public IsoAgLib::ProcessDataChangeHandler_c {
     public:
       TutorialDataSource_c();
       ~TutorialDataSource_c();
@@ -26,6 +30,9 @@ namespace IsoAgLibTutorialDataSource {
       virtual void handleNewWorkStateData( bool ab_on );
       virtual void handleNewSensorAData( int32_t ai32_newData );
       virtual void handleNewSensorBData( int32_t ai32_newData );
+
+      /* IsoAgLib::ProcessDataChangeHandler_c */
+	  virtual bool processDefaultLoggingStart( IsoAgLib::EventSource_c ac_src, int32_t ai32_val, const IsoAgLib::iIsoName_c& ac_callerIsoName );
   };
 
 } /* namespace IsoAgLibTutorialDataSource */
