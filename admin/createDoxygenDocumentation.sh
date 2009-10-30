2aae5a59574bb6df99f0458f729289f92672f086
Martin Wodok <m.wodok@osb-ag.de>
2009-10-30 10:12:11 +0000

    [Cleanup] [Admin-Scripts/Docu-Texts] [removed installation.txt/download.txt/contact_and_support.txt and adapted admin-scripts]
    Post-review on release

---
+++ admin/createDoxygenDocumentation.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd `dirname $0`
+cd ../..
+
+# A) remove old DOC
+find doc/ -name "*.html" -o -name "*.png" -o -name "*.md5" -o -name "*.map" -exec rm -f {} \;
+
+# B) Create Doc
+doxygen doc/DoxyfileAll
+doxygen doc/DoxyfileApi
