9e2dcd15dee6881abd0afed571f6a56c2bedc505
Christoph Staub <c.staub@osb-ag.de>
2009-12-14 14:48:38 +0000

    [Enhancement] [HAL AMS5] [Additional handling for ZMDLs] [Bugfix] [HAL AMS5] [Fixed channel-offset]
    - sensor.h: Added #ifndef statements to allow define-overwriting by project-defines
    - getCounterLastSignalAge: Fixed channel-offset
    
    Done by Christoph Staub
    Review by Martin Wodok

--- library/xgpl_src/supplementary_driver/hal/ams5/actor/actor_target_extensions.cpp
+++ library/xgpl_src/supplementary_driver/hal/ams5/actor/actor_target_extensions.cpp
@@ -2,11 +2,18 @@
     actor_target_extension.cpp - __HAL namespace layer for AMS5
 
     -------------------
-    date                 : 18.06.2007
-    copyright            : (c) 2007 GESAS GmbH
+    date                 : 09.09.2008
+    copyright            : (c) 2007/2008 GESAS GmbH
     email                : stefan.klueh@gesas:de
     type                 : Module
  ***************************************************************************/
+/*   History:
+              18.06.2007 V1.00  - first release
+
+              09.09.2008 V1.01  - revision of function "getMaxDigout" and "setDigout":
+                                  AMS5 with decentral periphery (ZMDL/LW)
+*/
+// ---------------------------------------------------------------------------------------------
 
 
 #include "actor_target_extensions.h"
@@ -23,31 +30,40 @@ namespace __HAL
 
       Use symbolic output channels form "config.h"!
  
-      @param aui8_channel channel number of output
+      @param rui8_channel channel number of output
       @return max possible value
    */
-   uint16_t getMaxDigout(uint8_t aui8_channel)
+   uint16_t getMaxDigout(uint8_t rui8_channel)
    {
-      if (aui8_channel < 32)   // static digital outputs (A6833)
+      if (rui8_channel < 32)   // static digital outputs (A6833)
       {
          return 1;
       }
       else
       {
-         switch (aui8_channel)
+         switch (rui8_channel)
          {
             case 32:
             case 33:
             case 34:
                return 65535;   // PWM value for analog output
 
-               case 35:
-               case 36:
+            case 35:
+            case 36:
                return 3;       // motor H-bridge state
 
-               default:
-               return 0;
-            }
+            default:
+               break;
+         }
+      }
+
+      if (rui8_channel < 181)   // decentral periphery (ZMDL/LW)
+      {
+         return 1;
+      }
+      else
+      {
+         return 0;
       }
    }
 
@@ -57,15 +73,30 @@ namespace __HAL
       No AMS5-BIOS channel is a "PWM" channel!
       Use symbolic output channels form "config.h"!
 
-      @param aui8_channel channel number of output
-      @param wValue range depends on channel!
+      @param  rui8_channel channel number of output
+      @param  wValue range depends on channel!
       @return HAL_NO_ERR
               HAL_RANGE_ERR  value is out of range
               HAL_CONFIG_ERR channel is not availabel 
    */
-   int16_t setDigout(uint8_t aui8_channel, uint16_t wValue)
+   int16_t setDigout(uint8_t rui8_channel, uint16_t wValue)
    {
-      return AMSBIOS::io_setOutput(aui8_channel, wValue);
+      if (rui8_channel < 37)
+      {     
+         return AMSBIOS::io_setOutput(rui8_channel, wValue);
+      }
+      else if (rui8_channel < 181)
+      {
+         // output channel number: 0..12
+         // Node number:           1..8
+         uint8_t NodeNr    = (rui8_channel-37)/18+1;
+         uint8_t ChannelNr = (rui8_channel-37)%18;
+         return AMSBIOS::ioExt_setOutput(NodeNr, ChannelNr, wValue);
+      }
+      else
+      {
+         return HAL_CONFIG_ERR;
+      }
    }
 
    /*@}*/
--- library/xgpl_src/supplementary_driver/hal/ams5/sensor/sensor_target_extensions.cpp
+++ library/xgpl_src/supplementary_driver/hal/ams5/sensor/sensor_target_extensions.cpp
@@ -2,15 +2,30 @@
     sensor_target_extensions.cpp - AMS5 specific extensions
 
     -------------------
-    date                 : 18.06.2007
-    copyright            : (c) 2007 GESAS GmbH
+    date                 : 09.09.2008
+    copyright            : (c) 2007/2008 GESAS GmbH
     email                : stefan.klueh@gesas:de
     type                 : Module
  ***************************************************************************/
+/*   History:
+              18.06.2007 V1.00  - first release
+
+              04.08.2008 V1.01  - correction of function "initCounter" and
+                                  "getCounterLastSignalAge":
+                                  wrong distinction between the interrupt input channels
+
+              09.09.2008 V1.02  - revision of function "get_digin_onoff" and "get_digin_onoff":
+                                  AMS5 with decentral periphery (ZMDL/LW)
+*/
+// ---------------------------------------------------------------------------------------------
+
 
 #include "sensor_target_extensions.h"
 #include <..\commercial_BIOS\bios_ams5\ams_bios.h>
-   
+
+//#include <supplementary_driver/driver/rs232/impl/rs232io_c.h>
+//char str_outputrs[20];
+
 /** time of last interrupt */
 __near static uint32_t gw_LastSignalIrq[3] = { 0, 0, 0 };
 
@@ -44,31 +59,31 @@ namespace __HAL
       init counter inputs
 
       Use symbolic counter input channels form "config.h"!
-      For "event counter mode" set aui16_timebase to "0".
+      For "event counter mode" set rui16_timebase to "0".
 
-      @param ab_channel input channel to use 
-      @param aui16_timebase timebase to calculate periods, frequency
+      @param rb_channel input channel to use 
+      @param rui16_timebase timebase to calculate periods, frequency
                             should be at least longer than longest
                             awaited signal period [msec.]
-      @param ab_activHigh true -> counter input is configured fo ACTIV_HIGH; else ACTIV_LOW
-      @param ab_risingEdge true -> counter triggers on rising edge; else on falling edge
+      @param rb_activHigh true -> counter input is configured fo ACTIV_HIGH; else ACTIV_LOW
+      @param rb_risingEdge true -> counter triggers on rising edge; else on falling edge
       @return HAL_NO_ERR if no error occured
    */
-   int16_t init_counter(uint8_t ab_channel, uint16_t aui16_timebase, bool ab_activHigh, bool ab_risingEdge)
+   int16_t init_counter(uint8_t rb_channel, uint16_t rui16_timebase, bool rb_activHigh, bool rb_risingEdge)
    {
-      if (ab_channel < 5)
+      if (rb_channel < 5)
       {
          // init counter 0 to 4 (cntX19_2, cntX19_3, cntX19_4, cntX20_3, cntX20_4)
-         return AMSBIOS::io_initCounter(ab_channel, aui16_timebase, ab_risingEdge);
+         return AMSBIOS::io_initCounter(rb_channel, rui16_timebase, rb_risingEdge);
       }
-      else if (ab_channel < 8)
+      else if (rb_channel < 8)
       {
-         // init interrupt input channel cntX19_5,  cntX19_6,  cntX21_5  
-         ab_channel -= 4;
-         int16_t retVal = AMSBIOS::io_initIrqInputs(ab_channel, g_IrqFunction[ab_channel], ab_risingEdge);
+         // init interrupt input channels cntX19_5,  cntX19_6,  cntX21_5  
+         rb_channel -= 5;
+         int16_t retVal = AMSBIOS::io_initIrqInputs(rb_channel, g_IrqFunction[rb_channel], rb_risingEdge);
          // enable interrupt
          if (retVal == HAL_NO_ERR)
-           return AMSBIOS::io_setIrqInputs(ab_channel, 1);
+           return AMSBIOS::io_setIrqInputs(rb_channel, 1);
          else
             return retVal;
       }
@@ -83,14 +98,14 @@ namespace __HAL
 
       Use symbolic counter input channels form "config.h"!
 
-      @param ab_channel channel of counter
+      @param rb_channel channel of counter
       @return counter events since init or last reset
    */
-   uint32_t getCounter(uint8_t ab_channel)
+   uint32_t getCounter(uint8_t rb_channel)
    {
       ioCounterValues_t CounterValues;
 
-      if (AMSBIOS::io_getCounterValues(ab_channel, &CounterValues) == HAL_NO_ERR)
+      if (AMSBIOS::io_getCounterValues(rb_channel, &CounterValues) == HAL_NO_ERR)
          return CounterValues.Counts;
       else
          return 0;
@@ -101,12 +116,12 @@ namespace __HAL
 
       Use symbolic counter input channels form "config.h"!
 
-      @param ab_channel channel of counter
-      @return C_NO_ERR ; HAL_RANGE_ERR if counter for ab_channel isn´t configured properly
+      @param rb_channel channel of counter
+      @return C_NO_ERR ; HAL_RANGE_ERR if counter for rb_channel isn´t configured properly
    */
-   int16_t resetCounter(uint8_t ab_channel)
+   int16_t resetCounter(uint8_t rb_channel)
    {
-     return AMSBIOS::io_resetCounter(ab_channel);
+     return AMSBIOS::io_resetCounter(rb_channel);
    }
 
    /**
@@ -114,15 +129,15 @@ namespace __HAL
 
       Use symbolic counter input channels form "config.h"!
 
-      @param ab_channel channel of counter
+      @param rb_channel channel of counter
       @return time between last two signals or 0xFFFF if time is longer than initially
               given timebase
    */
-   uint16_t getCounterPeriod(uint8_t ab_channel)
+   uint16_t getCounterPeriod(uint8_t rb_channel)
    {
       ioCounterValues_t CounterValues;
 
-      if (AMSBIOS::io_getCounterValues(ab_channel, &CounterValues) == HAL_NO_ERR)
+      if (AMSBIOS::io_getCounterValues(rb_channel, &CounterValues) == HAL_NO_ERR)
       {
          return CounterValues.Period;         
       }
@@ -137,15 +152,15 @@ namespace __HAL
 
       Use symbolic counter input channels form "config.h"!
 
-      @param ab_channel channel of counter
+      @param rb_channel channel of counter
       @return frequency calculated from time between last two signals
               or 0 if time is longer than initially given timebase
    */
-   uint16_t getCounterFrequency(uint8_t ab_channel)
+   uint16_t getCounterFrequency(uint8_t rb_channel)
    {
       ioCounterValues_t CounterValues;
 
-      if (AMSBIOS::io_getCounterValues(ab_channel, &CounterValues) == HAL_NO_ERR)
+      if (AMSBIOS::io_getCounterValues(rb_channel, &CounterValues) == HAL_NO_ERR)
       {
          return CounterValues.Frequency;         
       }
@@ -163,10 +178,10 @@ namespace __HAL
 
       !!! this is a "counter function" -> use symbolic counter input channels only !!!
 
-      @param ab_channel channel of counter
+      @param rb_channel channel of counter
       @return time since last signal [msec.]
    */
-   uint32_t getCounterLastSignalAge(uint8_t ab_channel)
+   uint32_t getCounterLastSignalAge(uint8_t rb_channel)
    {
       uint32_t i32_now = AMSBIOS::sys_getSystemTimer();
 
@@ -174,10 +189,10 @@ namespace __HAL
       uint32_t          LastTimestamp;
       int32_t           i32_retValue;
 
-      if (ab_channel < 4)
+      if (rb_channel < 5)
       {
           // counter 0 to 4 (cntX19_2, cntX19_3, cntX19_4, cntX20_3, cntX20_4)
-         if (AMSBIOS::io_getCounterValues(ab_channel, &CounterValues) == HAL_NO_ERR)
+         if (AMSBIOS::io_getCounterValues(rb_channel, &CounterValues) == HAL_NO_ERR)
          {
             // calculate signal age
             if (i32_now > CounterValues.Timestamp)
@@ -188,19 +203,19 @@ namespace __HAL
          else
             return 0;
       }
-      else if (ab_channel < 7)
+      else if (rb_channel < 8)
       {
-         // interrupt input channel cntX19_5,  cntX19_6,  cntX21_5  
-         ab_channel -= 4;
+         // interrupt input channels cntX19_5,  cntX19_6,  cntX21_5  
+         rb_channel -= 5;
          // disable interrupt
-         i32_retValue = AMSBIOS::io_setIrqInputs(ab_channel, 0);
+         i32_retValue = AMSBIOS::io_setIrqInputs(rb_channel, 0);
 
-         LastTimestamp = gw_LastSignalIrq[ab_channel];
+         LastTimestamp = gw_LastSignalIrq[rb_channel];
 
          if (i32_retValue == HAL_NO_ERR)
          {
             // enable interrupt
-            AMSBIOS::io_setIrqInputs(ab_channel, 1);
+            AMSBIOS::io_setIrqInputs(rb_channel, 1);
          }
          else
          {
@@ -226,14 +241,14 @@ namespace __HAL
       0...1023 = 0...2500mV
       !!! remove jumper corresponding to channel !!!     
 
-      @param ab_channel measured channel
+      @param rb_channel measured channel
       @return voltage [0..2500] [mV] or HAL_RANGE_ERR on wrong input channel number
    */
-   int16_t getAdcVoltage(uint8_t ab_channel)
+   int16_t getAdcVoltage(uint8_t rb_channel)
    {
-      if (ab_channel>7) return HAL_RANGE_ERR;
+      if (rb_channel>7) return HAL_RANGE_ERR;
 
-      int32_t i32_temp = AMSBIOS::io_getADC(ab_channel);
+      int32_t i32_temp = AMSBIOS::io_getADC(rb_channel);
       if ( i32_temp == (int32_t)HAL_RANGE_ERR ) return HAL_RANGE_ERR;
       else return ( (int16_t)( i32_temp * 2500l/1023l ) );
    }
@@ -244,14 +259,14 @@ namespace __HAL
       0...1023 = 0...2500mV
       !!! remove jumper corresponding to channel !!!     
 
-      @param ab_channel measured channel
+      @param rb_channel measured channel
       @return voltage [0..2500] [mV] or HAL_RANGE_ERR on wrong input channel number
    */
-   int16_t getAdcMeanVoltage(uint8_t ab_channel)
+   int16_t getAdcMeanVoltage(uint8_t rb_channel)
    {
-      if (ab_channel>7) return HAL_RANGE_ERR;
+      if (rb_channel>7) return HAL_RANGE_ERR;
 
-      int32_t i32_temp = AMSBIOS::io_getADCMean(ab_channel);
+      int32_t i32_temp = AMSBIOS::io_getADCMean(rb_channel);
       if ( i32_temp == (int32_t)HAL_RANGE_ERR ) return HAL_RANGE_ERR;
       else return ( (int16_t)( i32_temp * 2500l/1023l ) );
    }
@@ -262,14 +277,14 @@ namespace __HAL
       0...957 = 0...20mA
       !!! set jumper corresponding to channel in position 2-3 !!!     
 
-      @param ab_channel measured channel
+      @param rb_channel measured channel
       @return current [0..20000] [uA] or HAL_RANGE_ERR on wrong input channel number
    */
-   int16_t  getAdcCurrent(uint8_t ab_channel)
+   int16_t  getAdcCurrent(uint8_t rb_channel)
    {
-      if (ab_channel>7) return HAL_RANGE_ERR;
+      if (rb_channel>7) return HAL_RANGE_ERR;
 
-      int32_t i32_temp = AMSBIOS::io_getADC(ab_channel);
+      int32_t i32_temp = AMSBIOS::io_getADC(rb_channel);
       if ( i32_temp == (int32_t)HAL_RANGE_ERR ) return HAL_RANGE_ERR;
       else return ( (int16_t)( i32_temp * 20000l/957l ) );
    }
@@ -280,14 +295,14 @@ namespace __HAL
       0...957 = 0...20mA
       !!! set jumper corresponding to channel in position 2-3 !!!     
 
-      @param ab_channel measured channel
+      @param rb_channel measured channel
       @return current [0..20000] [uA] or HAL_RANGE_ERR on wrong input channel number
    */
-   int16_t  getAdcMeanCurrent(uint8_t ab_channel)
+   int16_t  getAdcMeanCurrent(uint8_t rb_channel)
    {
-      if (ab_channel>7) return HAL_RANGE_ERR;
+      if (rb_channel>7) return HAL_RANGE_ERR;
 
-      int32_t i32_temp = AMSBIOS::io_getADCMean(ab_channel);
+      int32_t i32_temp = AMSBIOS::io_getADCMean(rb_channel);
       if ( i32_temp == (int32_t)HAL_RANGE_ERR ) return HAL_RANGE_ERR;
       else return ( (int16_t)( i32_temp * 20000l/957l ) );
    }
@@ -299,18 +314,32 @@ namespace __HAL
       - analog inputs used as digital inputs (X5/X6)
       - counter and interrupt inputs used as digital inputs (X19/X20)
       - DIP switches (SW1)
-
+      - static digital inputs from decentral periphery (ZMDL/LW)
+      
       !!! use symbolic input channels only !!!
       !!! analog input: set jumper corresponding to channel in position 1-2 !!!     
 
-      @param bInputNumber input channel number
+      @param rb_channelNumber input channel number
       @return ON, OFF or HAL_RANGE_ERR
    */
-   int16_t get_digin_onoff(uint8_t bInputNumber)
+   int16_t get_digin_onoff(uint8_t rb_channelNumber)
    {
-      if (bInputNumber > 39) return HAL_RANGE_ERR;
-      
-      return AMSBIOS::io_getDigin(bInputNumber);
+      if (rb_channelNumber < 40)
+      {     
+        return AMSBIOS::io_getDigin(rb_channelNumber);
+      }
+      else if (rb_channelNumber < 144)
+      {
+         // input channel number: 0..12
+         // Node number:          1..8
+         uint8_t NodeNr    = (rb_channelNumber-40)/13+1;
+         uint8_t ChannelNr = (rb_channelNumber-40)%13;
+         return AMSBIOS::ioExt_getDigin(NodeNr, ChannelNr);
+      }
+      else
+      {
+         return HAL_RANGE_ERR;
+      }
    }
 
    /**
@@ -320,18 +349,32 @@ namespace __HAL
       - analog inputs used as digital inputs (X5/X6)
       - counter and interrupt inputs used as digital inputs (X19/X20)
       - DIP switches (SW1)
+      - static digital inputs from decentral periphery (ZMDL/LW)
 
       !!! use symbolic input channels only !!!
       !!! analog input: set jumper corresponding to channel in position 1-2 !!!     
 
-      @param bInputNumber input channel number
+      @param rb_channelNumber input channel number
       @return ON, OFF or HAL_RANGE_ERR
    */
-   int16_t get_digin_onoff_static(uint8_t bInputNumber)
+   int16_t get_digin_onoff_static(uint8_t rb_channelNumber)
    {
-      if (bInputNumber > 39) return HAL_RANGE_ERR;
-
-      return AMSBIOS::io_getDiginStatic(bInputNumber);
+      if (rb_channelNumber < 40)
+      {     
+         return AMSBIOS::io_getDiginStatic(rb_channelNumber);
+      }
+      else if (rb_channelNumber < 144)
+      {
+         // input channel number: 0..12
+         // Node number:          1..8
+         uint8_t NodeNr    = (rb_channelNumber-40)/13+1;
+         uint8_t ChannelNr = (rb_channelNumber-40)%13;
+         return AMSBIOS::ioExt_getDigin(NodeNr, ChannelNr);
+      }
+      else
+      {
+         return HAL_RANGE_ERR;
+      }
    }
 
    /*@}*/
