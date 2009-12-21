ec663b1615b706aacd52a192ea893ad380ebe3bb
Martin Wodok <m.wodok@osb-ag.de>
2009-12-21 10:58:55 +0000

    [Change] #682 [update_makefile.sh] [Makefile is now again the Makefile itself, not a linking shortcut]
    Post-review by Thomas Hafner

--- tools/project_generation/update_makefile.sh
+++ tools/project_generation/update_makefile.sh
@@ -11,49 +11,6 @@
 # ####################################################################### #
 
 # ####################################################################### #
-#                                                                         #
-# This file is part of the "IsoAgLib", an object oriented program library #
-# to serve as a software layer between application specific program and   #
-# communication protocol details. By providing simple function calls for  #
-# jobs like starting a measuring program for a process data value on a    #
-# remote ECU, the main program has not to deal with single CAN telegram   #
-# formatting. This way communication problems between ECU's which use     #
-# this library should be prevented.                                       #
-# Everybody and every company is invited to use this library to make a    #
-# working plug and play standard out of the printed protocol standard.    #
-#                                                                         #
-# Copyright (C) 2000 - 2004 Dipl.-Inform. Achim Spangler                  #
-#                                                                         #
-# The IsoAgLib is free software; you can redistribute it and/or modify it #
-# under the terms of the GNU General Public License as published          #
-# by the Free Software Foundation; either version 2 of the License, or    #
-# (at your option) any later version.                                     #
-#                                                                         #
-# This library is distributed in the hope that it will be useful, but     #
-# WITHOUT ANY WARRANTY; without even the implied warranty of              #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       #
-# General Public License for more details.                                #
-#                                                                         #
-# You should have received a copy of the GNU General Public License       #
-# along with IsoAgLib; if not, write to the Free Software Foundation,     #
-# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA           #
-#                                                                         #
-# As a special exception, if other files instantiate templates or use     #
-# macros or inline functions from this file, or you compile this file and #
-# link it with other works to produce a work based on this file, this file#
-# does not by itself cause the resulting work to be covered by the GNU    #
-# General Public License. However the source code for this file must still#
-# be made available in accordance with section (3) of the                 #
-# GNU General Public License.                                             #
-#                                                                         #
-# This exception does not invalidate any other reasons why a work based on#
-# this file might be covered by the GNU General Public License.           #
-#                                                                         #
-# Alternative licenses for IsoAgLib may be arranged by contacting         #
-# the main author Achim Spangler by a.spangler@osb-ag:de                  #
-# ####################################################################### #
-
-# ####################################################################### #
 # This file contains several helper functions to create
 # project specific Makefiles based on several config variable
 # settings. The vars are used to defined the features which are
@@ -1517,10 +1474,17 @@ shortcut_makefile()
     local TARGET="$1"
     local SHORTCUT="$2"
 
+#### By default, the long version is moved to the shortcut version now.
     rm -f "$SHORTCUT"
-    cat <<END_OF_MAKEFILE >"$SHORTCUT"
-include $TARGET
-END_OF_MAKEFILE
+    mv "$TARGET" "$SHORTCUT"
+
+#### This is the shortcut version (currently with "include Makefile__x__y")
+#### which is currently not used anymore. It could be reactivated, but then
+#### optional with some cmdline-parameter to enable it.
+###########################################################################
+#    cat <<END_OF_MAKEFILE >"$SHORTCUT"
+#include $TARGET
+#END_OF_MAKEFILE
 }
 
 create_standard_makefile()
