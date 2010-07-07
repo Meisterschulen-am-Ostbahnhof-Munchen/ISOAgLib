581508ba4d4bb1cff251fecf4df7d1018cfcd7df
Martin Wodok <m.wodok@osb-ag.de>
2010-07-07 11:37:48 +0000

    [Compilefix] [IsoMonitor] r4587/4589/4591/4595/#1033 [Printouts in DEBUG_ISOMONITOR fixed]
    Postreview by T. Hafner

--- library/xgpl_src/IsoAgLib/comm/Part5_NetworkManagement/impl/isomonitor_c.cpp
+++ library/xgpl_src/IsoAgLib/comm/Part5_NetworkManagement/impl/isomonitor_c.cpp
@@ -1127,7 +1127,7 @@ bool IsoMonitor_c::processMsg()
             *pc_itemSameISOName = NULL;
 
 #if DEBUG_ISOMONITOR
-  std::cout << std::endl << "IsoMonitor_c::processMsg()-BEGIN" << std::endl;
+  INTERNAL_DEBUG_DEVICE << INTERNAL_DEBUG_DEVICE_ENDL << "IsoMonitor_c::processMsg()-BEGIN" << INTERNAL_DEBUG_DEVICE_ENDL;
   debugPrintNameTable();
 #endif
 
@@ -1386,7 +1386,7 @@ bool IsoMonitor_c::processMsg()
   } // end switch for NON-DESTINATION pgn
 
 #if DEBUG_ISOMONITOR
-  std::cout << "IsoMonitor_c::processMsg()-END" << std::endl;
+  INTERNAL_DEBUG_DEVICE << "IsoMonitor_c::processMsg()-END" << INTERNAL_DEBUG_DEVICE_ENDL;
   debugPrintNameTable();
 #endif
 
