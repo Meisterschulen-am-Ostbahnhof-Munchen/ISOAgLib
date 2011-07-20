5cb7c6928c65d59530da6e727003a85ea2d0d63a
Martin Wodok <m.wodok@osb-ag.de>
2011-07-20 09:44:13 +0000

    [Compilefix] [prev.commit] [Multiton-Instance for ProcessPkg unused.]
    No review needed.

--- library/xgpl_src/IsoAgLib/comm/Part10_TaskController_Client/impl/processpkg_c.cpp
+++ library/xgpl_src/IsoAgLib/comm/Part10_TaskController_Client/impl/processpkg_c.cpp
@@ -40,8 +40,8 @@ ProcessPkg_c::ProcessPkg_c( const CanPkg_c& arc_src, int ai_multitonInst )
 }
 
 /** default constructor which has nothing to do */
-ProcessPkg_c::ProcessPkg_c( int ai_multitonInst )
-  : CanPkgExt_c( ai_multitonInst )
+ProcessPkg_c::ProcessPkg_c( int /*ai_multitonInst*/ )
+  : CanPkgExt_c( )
   , mi32_pdValue( 0 )
   , bit_data()
 {
