13ae93e4d5816647bf8cefd23308ecf8e75c20c6
Martin Wodok <m.wodok@osb-ag.de>
2012-07-10 15:50:16 +0000

    [Compilefix] [prev.commit] [...]
    No review needed.

--- library/xgpl_src/IsoAgLib/comm/Part5_NetworkManagement/impl/saclaimhandler_c.h
+++ library/xgpl_src/IsoAgLib/comm/Part5_NetworkManagement/impl/saclaimhandler_c.h
@@ -36,21 +36,18 @@ class ControlFunctionStateHandler_c
 public:
 
   enum iIsoItemAction_e { /// ATTENTION: If a node is added and "AddToMonitorList" is broadcast,
-    /// ~~~~~~~~~  the node doesn't need to have a valid Address at this moment!!
-    AddToMonitorList,        /// Initial Address-Claim --> Node added to IsoMonitor
-    ChangedAddress,          /// Changed Address
-    LostAddress,             /// Address stolen due to Address-Claim of other node with Same SA (New Address is 254)
-    ReclaimedAddress,        /// Reclaimed an Address after Address was stolen (Old Address was 254)
-    RemoveFromMonitorList    /// No more Address-Claims (after requests) --> Node removed from IsoMonitor
+                          /// ~~~~~~~~~  the node doesn't need to have a valid Address at this moment!!
+    AddToMonitorList,     /// Initial Address-Claim --> Node added to IsoMonitor
+    ChangedAddress,       /// Changed Address
+    LostAddress,          /// Address stolen due to Address-Claim of other node with Same SA (New Address is 254)
+    ReclaimedAddress,     /// Reclaimed an Address after Address was stolen (Old Address was 254)
+    RemoveFromMonitorList /// No more Address-Claims (after requests) --> Node removed from IsoMonitor
   };
 
   ControlFunctionStateHandler_c() {}
   virtual ~ControlFunctionStateHandler_c() {}
 
-  /** this function is called by IsoMonitor_c on addition, state-change and removal of an IsoItem.
-      <!-- @param at_action enumeration indicating what happened to this IsoItem. @see IsoItemModification_en / IsoItemModification_t
-      @param acrc_isoItem reference to the (const) IsoItem which is changed (by existance or state)-->
-  */
+  /** this function is called by IsoMonitor_c on addition, state-change and removal of an IsoItem. */
   virtual void reactOnIsoItemModification(
       iIsoItemAction_e ae_action,
       IsoItem_c const &acrc_isoItem) = 0;
@@ -71,7 +68,7 @@ public:
 
 private:
   virtual void reactOnIsoItemModification(
-      IsoItemModification_t at_action,
+      iIsoItemAction_e at_action,
       IsoItem_c const &acrc_isoItem)
   {
     mrt_owner.reactOnIsoItemModification(at_action, acrc_isoItem);
