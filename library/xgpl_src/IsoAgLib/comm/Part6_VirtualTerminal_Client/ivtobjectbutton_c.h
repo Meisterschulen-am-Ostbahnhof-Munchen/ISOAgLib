856d635abd9398a642fbb5b38b0633c84c788c46
Martin Wodok <m.wodok@osb-ag.de>
2012-07-11 07:36:50 +0000

    [Compilefix] [VT-Client] [v4button]
    No review needed.

--- library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/ivtobjectbutton_c.h
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/ivtobjectbutton_c.h
@@ -50,78 +50,51 @@ public:
   bool v4select(uint8_t selectOrActivate) { return vtObject_c::select(selectOrActivate); }
 
   /// Operation: setSize
-  /// 
-  /// @param newWidth
-  /// @param newHeight
-  /// @param b_updateObject
   void setSize(uint16_t newWidth, uint16_t newHeight, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     vtObjectButton_c::setSize (newWidth, newHeight, b_updateObject, b_enableReplaceOfCmd);
   }
 
   /// Operation: setWidth
-  /// 
-  /// @param newValue
-  /// @param b_updateObject
   void setWidth(uint16_t newValue, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     vtObjectButton_c::setWidth (newValue, b_updateObject, b_enableReplaceOfCmd);
   }
 
   /// Operation: setHeight
-  ///
-  /// @param newValue
-  /// @param b_updateObject
   void setHeight(uint16_t newValue, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     vtObjectButton_c::setHeight (newValue, b_updateObject, b_enableReplaceOfCmd);
   }
 
   /// Operation: setBackgroundColour
-  ///
-  /// @param newValue
-  /// @param b_updateObject
   void setBackgroundColour(uint8_t newValue, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     vtObjectButton_c::setBackgroundColour (newValue, b_updateObject, b_enableReplaceOfCmd);
   }
 
   //  Operation: setBorderColour
-  //! Parameter:
-  //! @param newValue:
-  //! @param b_updateObject:
   void setBorderColour(uint8_t newValue, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     vtObjectButton_c::setBorderColour (newValue, b_updateObject, b_enableReplaceOfCmd);
   }
 
   //  Operation: setKeyCode
-  //! Parameter:
-  //! @param newValue:
-  //! @param b_updateObject:
   void setKeyCode(uint8_t newValue, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     vtObjectButton_c::setKeyCode (newValue, b_updateObject, b_enableReplaceOfCmd);
   }
 
-  //  Operation: setOptions
-  //! Parameter:
-  //! @param newValue:
-  //! @param b_updateObject:
+  //  Operation: v4setOptions
+  //! Should only be used for v4 object-pools
   void v4setOptions(uint8_t newValue, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
-    vtObjectButton_c::setOptions (newValue, b_updateObject, b_enableReplaceOfCmd);
+    vtObjectButton_c::v4setOptions (newValue, b_updateObject, b_enableReplaceOfCmd);
   }
 
   //  Operation: moveChildLocation
-  //! Parameter:
-  //! @param apc_childObject:
-  //! @param dx:
-  //! @param dy:
   bool moveChildLocation(IsoAgLib::iVtObject_c* apc_childObject, int8_t dx, int8_t dy, bool b_updateObject=false) {
     return vtObjectButton_c::moveChildLocation(apc_childObject, dx, dy, b_updateObject);
   }
 
   //  Operation: setChildPosition
-  //! @param apc_childObject:
-  //! @param dx:
-  //! @param dy:
   bool setChildPosition(IsoAgLib::iVtObject_c* apc_childObject, int16_t dx, int16_t dy, bool b_updateObject=false, bool b_enableReplaceOfCmd=false) {
     return vtObjectButton_c::setChildPosition(apc_childObject, dx, dy, b_updateObject, b_enableReplaceOfCmd);
   }
+
 #ifdef USE_ISO_TERMINAL_GETATTRIBUTES
   // ///////////////////////// getter for attributes
   /** that attribute is in parentheses in the spec, so commented out here
