487307fb68cba0fef0c38867b03bd0037ea772ad
Martin Wodok <m.wodok@osb-ag.de>
2013-06-27 16:48:33 +0000

    [Enhancement] [isoaglib_assert] [GNU/WIN32 discard reentrant assertions]
    - Also allows a breakpoint to be set.
    
    Reviewed by M. Wodok & A. Kandera

--- library/xgpl_src/IsoAgLib/util/iassert.cpp
+++ library/xgpl_src/IsoAgLib/util/iassert.cpp
@@ -10,12 +10,32 @@
   Public License with exceptions for ISOAgLib. (See accompanying
   file LICENSE.txt or copy at <http://isoaglib.com/download/license>)
 */
-
 #include <IsoAgLib/isoaglib_config.h>
 
-#if defined(__GNUC__) || defined(WIN32)
-// nothing to do: provided by standard libraries
-#elif !defined(NDEBUG)
+#if defined(NDEBUG)
+// nothing to do, assertions pre-processed out completely.
+#elif defined(__GNUC__) || defined(WIN32)
+
+#include "iassert.h"
+
+static bool alreadyInAssert = false;
+
+bool isoaglib_assert_callStart()
+{
+  // This is just a function to set a breakpoint to catch isoaglib_assert calls
+
+  bool retval = alreadyInAssert;
+  alreadyInAssert = true;
+
+  return retval;
+}
+
+void isoaglib_assert_callFinish()
+{
+  alreadyInAssert = false;
+}
+
+#else
 
 #include "iassert.h"
 #include <cstdlib>
