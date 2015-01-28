ba64c2626871b55366ca3c0e78712c1b70743de8
Martin Wodok <m.wodok@osb-ag.de>
2015-01-28 09:30:55 +0000

    [Bugfix] [HAL-Simulator] [Wrong channel used in ENTRY_POINT_FOR_RECEIVE_CAN_MSG / ENTRY_POINT_FOR_SEND_CAN_MSG]
    - Fixed. Problem was that not only the "channel" is being checked,
      but all channels, so the resulting channel needs to be used!
    
    Reviewed by M. Wodok

--- library/xgpl_src/IsoAgLib/hal/pc/can/can_driver_canserver_socket_hal_simulator.cpp
+++ library/xgpl_src/IsoAgLib/hal/pc/can/can_driver_canserver_socket_hal_simulator.cpp
@@ -16,7 +16,7 @@
   file LICENSE.txt or copy at <http://isoaglib.com/download/license>)
 */
 
-// See if the HALSimulator_c wants to insert a can msg for this bBusNumber and bMsgObj
+// See if the HALSimulator_c wants to insert a can msg for this channel
 #define ENTRY_POINT_FOR_INSERT_RECEIVE_CAN_MSG                                                            \
 {                                                                                                         \
   uint8_t bBusNumber = channel;                                                                           \
@@ -46,10 +46,10 @@
 
 
 #define ENTRY_POINT_FOR_RECEIVE_CAN_MSG \
-  __HAL::halSimulator().ReceiveCanMsg( channel, s_transferBuf.s_data.i32_sendTimeStamp, (msg.identType() == __IsoAgLib::Ident_c::ExtendedIdent ) ? 1 : 0, msg.ident(), msg.getLen(), msg.getUint8DataPointer() );
+  __HAL::halSimulator().ReceiveCanMsg( s_transferBuf.s_data.ui8_bus, s_transferBuf.s_data.i32_sendTimeStamp, (msg.identType() == __IsoAgLib::Ident_c::ExtendedIdent ) ? 1 : 0, msg.ident(), msg.getLen(), msg.getUint8DataPointer() );
 
 #define ENTRY_POINT_FOR_SEND_CAN_MSG \
-  __HAL::halSimulator().SendCanMsg( channel, s_transferBuf.s_data.i32_sendTimeStamp, (msg.identType() == __IsoAgLib::Ident_c::ExtendedIdent ) ? 1 : 0, msg.ident(), msg.getLen(), msg.getUint8DataConstPointer() );
+  __HAL::halSimulator().SendCanMsg( s_transferBuf.s_data.ui8_bus, s_transferBuf.s_data.i32_sendTimeStamp, (msg.identType() == __IsoAgLib::Ident_c::ExtendedIdent ) ? 1 : 0, msg.ident(), msg.getLen(), msg.getUint8DataConstPointer() );
 
   
 #include "can_driver_canserver_socket.cpp"
