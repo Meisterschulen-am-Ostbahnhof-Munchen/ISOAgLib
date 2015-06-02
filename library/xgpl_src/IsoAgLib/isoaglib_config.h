ac334a8c25149ddfdf6245c52d91705f3f766f3a
Martin Wodok <m.wodok@osb-ag.de>
2015-06-02 11:29:57 +0000

    [Release] [2.7.2] [Changelog + FINAL Release]

--- library/xgpl_src/IsoAgLib/isoaglib_config.h
+++ library/xgpl_src/IsoAgLib/isoaglib_config.h
@@ -24,7 +24,7 @@
 #define ISOAGLIB_VERSION_MAJOR 2
 #define ISOAGLIB_VERSION_MINOR 7
 #define ISOAGLIB_VERSION_PATCH 2
-#define ISOAGLIB_VERSION_RC 3
+#define ISOAGLIB_VERSION_RC 0
 // RC = 0 => Final Release.
 // RC = 1 => Release Candidate 1, etc.
 
