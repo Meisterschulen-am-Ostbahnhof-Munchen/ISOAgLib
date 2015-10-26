ee1405d54085f988f7241b5f29347151f9285957
Martin Wodok <m.wodok@osb-ag.de>
2015-10-26 10:20:51 +0000

    [Release] [2.8.0] [Version Information in isoaglib_config.h adapted.]

--- library/xgpl_src/IsoAgLib/isoaglib_config.h
+++ library/xgpl_src/IsoAgLib/isoaglib_config.h
@@ -22,8 +22,8 @@
 #include <IsoAgLib/hal/hal_config.h>
 
 #define ISOAGLIB_VERSION_MAJOR 2
-#define ISOAGLIB_VERSION_MINOR 7
-#define ISOAGLIB_VERSION_PATCH 2
+#define ISOAGLIB_VERSION_MINOR 8
+#define ISOAGLIB_VERSION_PATCH 0
 #define ISOAGLIB_VERSION_RC 0
 // RC = 0 => Final Release.
 // RC = 1 => Release Candidate 1, etc.
