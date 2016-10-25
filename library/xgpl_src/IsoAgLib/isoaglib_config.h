684ba6e91562d9cfac0d01452f2590e2db44f418
Martin Wodok <m.wodok@osb-ag.de>
2016-10-25 14:29:52 +0000

    [Release] [2.8.2] [Release Candidate 1 preparations]
    Reviewed by M. Wodok

--- library/xgpl_src/IsoAgLib/isoaglib_config.h
+++ library/xgpl_src/IsoAgLib/isoaglib_config.h
@@ -23,9 +23,9 @@
 
 #define ISOAGLIB_VERSION_MAJOR 2
 #define ISOAGLIB_VERSION_MINOR 8
-#define ISOAGLIB_VERSION_PATCH 1
+#define ISOAGLIB_VERSION_PATCH 2
 #define ISOAGLIB_VERSION_FIX 0
-#define ISOAGLIB_VERSION_RC -1
+#define ISOAGLIB_VERSION_RC 1
 // RC < 0 => Changes in Trunk, preparing Next Release.
 // RC = 0 => Final Release.
 // RC = 1 => Release Candidate 1, etc.
