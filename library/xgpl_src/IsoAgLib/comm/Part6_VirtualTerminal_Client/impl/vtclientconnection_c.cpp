4fc8bf2d4b1f7c41d0bed2a52bd098ad9db38d9e
Burkhard Fischer <b.fischer@osb-ag.de>
2017-06-19 11:11:46 +0000

    [Fix] [VT-Client] [skip AUX-N functionalities, if objects are omitted from pool upload]
    reviewed by M. Wodok

--- library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/impl/vtclientconnection_c.cpp
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/impl/vtclientconnection_c.cpp
@@ -166,7 +166,10 @@ VtClientConnection_c::VtClientConnection_c(
     { // collect all available AUX 2 function objects in list
       IsoAgLib::iVtObjectAuxiliaryFunction2_c *aux2f = static_cast<IsoAgLib::iVtObjectAuxiliaryFunction2_c*>(p_obj);
       m_aux2Functions.getObjects()[ p_obj->getID() ] = aux2f;
-      auxNOptionsFunction.setBit( static_cast<AuxNOptions_t>( aux2f->getFunctionType() ) );
+      if (!aux2f->isOmittedFromUpload())
+      {
+        auxNOptionsFunction.setBit( static_cast<AuxNOptions_t>( aux2f->getFunctionType() ) );
+      }
     }
 #endif
 #ifdef USE_VTOBJECT_auxiliaryinput2
@@ -174,7 +177,10 @@ VtClientConnection_c::VtClientConnection_c(
     { // collect all available AUX 2 input objects in list
       IsoAgLib::iVtObjectAuxiliaryInput2_c *aux2i = static_cast<IsoAgLib::iVtObjectAuxiliaryInput2_c*>(p_obj);
       m_aux2Inputs.getObjectList().push_back( aux2i );
-      auxNOptionsInput.setBit( static_cast<AuxNOptions_t>( aux2i->getFunctionType() ) );
+      if (!aux2i->isOmittedFromUpload())
+      {
+        auxNOptionsInput.setBit( static_cast<AuxNOptions_t>( aux2i->getFunctionType() ) );
+      }
     }
 #endif
   }
