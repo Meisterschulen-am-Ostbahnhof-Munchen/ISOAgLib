21e6f2e5998524c7be7d6a4883fe66b1e021eb82
Martin Wodok <m.wodok@osb-ag.de>
2019-04-24 16:01:55 +0000

    [Clarification] [Licensing] [Made it explicitly clear about the commercial license]

--- library/xgpl_src/IsoAgLib/comm/Part3_ProprietaryMessages/impl/proprietarymessageclient_c.cpp
+++ library/xgpl_src/IsoAgLib/comm/Part3_ProprietaryMessages/impl/proprietarymessageclient_c.cpp
@@ -1,12 +1,19 @@
 /*
   proprietarymessageclient_c.cpp
 
-  (C) Copyright 2009 - 2019 by OSB AG and developing partners
+  (C) Copyright 2009 - 2019 by OSB AG
 
   See the repository-log for details on the authors and file-history.
   (Repository information can be found at <http://isoaglib.com/download>)
 
-  Use, modification and distribution are subject to the GNU General
+  Usage under Commercial License:
+  Licensees with a valid commercial license may use this file
+  according to their commercial license agreement. (To obtain a
+  commercial license contact OSB AG via <http://isoaglib.com/en/contact>)
+
+  Usage under GNU General Public License with exceptions for ISOAgLib:
+  Alternatively (if not holding a valid commercial license)
+  use, modification and distribution are subject to the GNU General
   Public License with exceptions for ISOAgLib. (See accompanying
   file LICENSE.txt or copy at <http://isoaglib.com/download/license>)
 */
