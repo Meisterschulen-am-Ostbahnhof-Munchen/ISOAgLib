92780a5d2a38a025f68c99207248f235b3e57be6
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-02-01 14:48:31 +0100

    ..

---
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/ivtclient_c.cpp
@@ -0,0 +1,16 @@
+/*
+ * ivtclient_c.cpp
+ *
+ *  Created on: 01.02.2022
+ *      Author: franz
+ */
+
+#include "ivtclient_c.h"
+
+namespace IsoAgLib {
+
+
+}
+
+
+
---
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/ivtclientconnection_c.cpp
@@ -0,0 +1,13 @@
+/*
+ * ivtclientconnection_c.cpp
+ *
+ *  Created on: 01.02.2022
+ *      Author: franz
+ */
+
+#include "ivtclientconnection_c.h"
+
+namespace IsoAgLib {
+
+
+}
