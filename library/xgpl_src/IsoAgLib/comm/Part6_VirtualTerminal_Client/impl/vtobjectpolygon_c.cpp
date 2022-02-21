5a41365fcc9d3fefea7f95fd0d072a6152fe135f
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-02-21 22:57:25 +0100

    this Code really works, tested by this example Code.

--- library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/impl/vtobjectpolygon_c.cpp
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/impl/vtobjectpolygon_c.cpp
@@ -84,9 +84,9 @@ struct vtObjectPolygon_c::iVtObjectPolygon_s: iVtObjectwMacro_s {
             , pointsToFollow(pointsToFollow)
     {
         while ((pointsToFollow[numberOfPoints].x != 65535)
-               &&
-               (pointsToFollow[numberOfPoints].y != 65535))
+           &&  (pointsToFollow[numberOfPoints].y != 65535))
             numberOfPoints++;
+        numberOfPoints--;
     }
 
 
