e724fb54547d9e2b74fa61ff0677d9c3efdeeeda
Franz Höpfinger <franz.hoepfinger@ms-muc.de>
2022-02-24 00:01:36 +0100

    ..

--- library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/impl/vtobjectpicturegraphic_c.h
+++ library/xgpl_src/IsoAgLib/comm/Part6_VirtualTerminal_Client/impl/vtobjectpicturegraphic_c.h
@@ -95,15 +95,16 @@ public:
     virtual void setRawData2 (uint8_t* newValue, uint32_t aui32_size, bool ab_rle, uint16_t aui16_actWidth=0xFFFF, uint16_t aui16_actHeight=0xFFFF, uint16_t aui16_width=0xFFFF);
 #ifdef CONFIG_USE_ISO_TERMINAL_GETATTRIBUTES
   // ///////////////////////// getter for attributes
-  virtual /** that attribute is in parentheses in the spec, so commented out here
+  /** that attribute is in parentheses in the spec, so commented out here
   uint8_t updateObjectType() const { return 20; }
   */
 
-  uint16_t updateWidth(bool b_SendRequest=false);
+    virtual uint16_t updateWidth(bool b_SendRequest=false);
+    virtual IsoAgLib::iVtObjectPictureGraphicOptions updateOptions(bool b_SendRequest=false);
+    virtual IsoAgLib::Colour updateTransparencyColour(bool b_SendRequest=false);
 
-        virtual IsoAgLib::iVtObjectPictureGraphicOptions updateOptions(bool b_SendRequest=false);
 
-        virtual IsoAgLib::Colour updateTransparencyColour(bool b_SendRequest=false);
+
   /** these attributes are in parentheses in the spec, so commented out here
   uint16_t updateActualWidth(bool b_SendRequest=false);
   uint16_t updateActualHeight(bool b_SendRequest=false);
